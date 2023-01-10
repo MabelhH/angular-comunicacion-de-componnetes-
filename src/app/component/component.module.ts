077b8114e8942989b5ddac967d05899f9557ddd6
clara23 <clarahuanaco20@gmail.com>
2023-01-10 10:30:42 -0500

    primera actualisacion

---
+++ src/app/component/component.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HijoComponent } from './hijo/hijo.component';
+
+
+
+@NgModule({
+  declarations: [
+    HijoComponent
+  ],
+  imports: [
+    CommonModule
+    
+  ],
+  exports:[
+    HijoComponent
+  ]
+})
+export class ComponentModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PadreComponent } from './padre/padre.component';
+import { ComponentModule } from '../component/component.module';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    PadreComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentModule,
+  ],
+  exports:[
+    PadreComponent
+  ]
+})
+export class PagesModule { }
